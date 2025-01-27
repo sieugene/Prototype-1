a762b31ee840b2079e8bd964fa02cf1501ee5ec5
sieugene <sieugene@mail.ru>
2025-01-27 22:32:31 +0900

    feat : wheel transform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private Variables
-    private float speed = 10.0f;
+    public float currentSpeed = 0.0f;
+    private float maxSpeed = 20.0f;
+    private float acceleration = 5.0f;
+    private float deceleration = 10.0f;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+
+    public Transform frontLeftTransform;
+    public Transform frontRightTransform;
+    public Transform rearLeftTransform;
+    public Transform rearRightTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +30,40 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        // We move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // If the forward or backward key is pressed, increase speed
+        if (forwardInput != 0)
+        {
+            currentSpeed += acceleration * Time.deltaTime * forwardInput;
+            currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeed, maxSpeed); // Limit speed
+        }
+        else
+        {
+            // If the keys are not pressed, the speed gradually decreases
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0, deceleration * Time.deltaTime);
+        }
+        // We move vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
         // We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        UpdateWheelTransforms(frontLeftTransform, isFrontWheel: true);
+        UpdateWheelTransforms(frontRightTransform, isFrontWheel: true);
+        UpdateWheelTransforms(rearLeftTransform, isFrontWheel: false);
+        UpdateWheelTransforms(rearRightTransform, isFrontWheel: false);
+
+    }
+
+    private void UpdateWheelTransforms(Transform wheelTransform, bool isFrontWheel)
+    {
+
+        wheelTransform.Rotate(Vector3.right, currentSpeed * Time.deltaTime * 360);
+
+
+        if (isFrontWheel)
+        {
+            Vector3 currentRotation = wheelTransform.localEulerAngles;
+            currentRotation.y = horizontalInput * 30f;
+            wheelTransform.localEulerAngles = currentRotation;
+        }
     }
 }
